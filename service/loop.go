0b50928a03eaab01be588653818412ee087272cc
fedelfeng <fedelfeng@tencent.com>
2022-11-17 01:27:11 +0800

    refactor: 参数校验

--- service/loop.go
+++ service/loop.go
@@ -7,12 +7,15 @@ import (
 	"github.com/fengde/gocommon/safex"
 )
 
-// 带函数锁执行函数，用于分布式场景，避免多开情况下，并发冲突
-func LoopWrapfWithLocker(fn func(), sleep time.Duration, lockId string, lockTimeout time.Duration) func() {
+// 带函数锁执行函数，用于分布式场景，避免多开情况下的并发冲突
+func LoopWrapfWithLocker(fn func(), interval time.Duration, lockId string, lockTimeout time.Duration) func() {
+	if interval == 0 || lockId == "" || lockTimeout/time.Second < 1 {
+		panic("入参非法")
+	}
 	return func() {
 		for {
 			global.Locker.Lock(lockId, int64(lockTimeout/time.Second), fn)
-			if !global.Continue(sleep) {
+			if !global.Continue(interval) {
 				break
 			}
 		}
@@ -20,11 +23,14 @@ func LoopWrapfWithLocker(fn func(), sleep time.Duration, lockId string, lockTime
 }
 
 // 循环执行函数
-func LoopWrapf(fn func(), sleep time.Duration) func() {
+func LoopWrapf(fn func(), interval time.Duration) func() {
+	if interval == 0 {
+		panic("入参非法")
+	}
 	return func() {
 		for {
 			safex.Func(fn)
-			if !global.Continue(sleep) {
+			if !global.Continue(interval) {
 				break
 			}
 		}
