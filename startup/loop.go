256a1905ee9dc6009ba1fe8e67077a13ce81c996
fedelfeng <fedelfeng@tencent.com>
2022-11-16 11:03:29 +0800

    feat: loop

---
+++ startup/loop.go
@@ -0,0 +1,32 @@
+package startup
+
+import (
+	"goserver/global"
+	"time"
+
+	"github.com/fengde/gocommon/safex"
+)
+
+// 带函数锁执行函数，用于分布式场景，避免多开情况下，并发冲突
+func LoopWrapfWithLocker(fn func(), sleep time.Duration, lockId string, lockTimeout time.Duration) func() {
+	return func() {
+		for {
+			global.Locker.Lock(lockId, int64(lockTimeout/time.Second), fn)
+			if !global.Continue(sleep) {
+				break
+			}
+		}
+	}
+}
+
+// 循环执行函数
+func LoopWrapf(fn func(), sleep time.Duration) func() {
+	return func() {
+		for {
+			safex.Func(fn)
+			if !global.Continue(sleep) {
+				break
+			}
+		}
+	}
+}
--- startup/startup.go
+++ startup/startup.go
@@ -1,11 +1,8 @@
 package startup
 
 import (
-	"goserver/global"
 	"goserver/service/serviceSentinel"
-	"time"
 
-	"github.com/fengde/gocommon/safex"
 	"github.com/fengde/gocommon/taskx"
 )
 
@@ -22,30 +19,6 @@ func NewStartup() *Startup {
 // 这里开机启动运行服务
 func (p *Startup) Run() {
 	p.g.Run(serviceSentinel.Run)
-}
-
-// 带函数锁执行函数，用于分布式场景，避免多开情况下，并发冲突
-func (p *Startup) LoopWrapfWithLocker(fn func(), sleep time.Duration, lockId string, lockTimeout time.Duration) func() {
-	return func() {
-		for {
-			global.Locker.Lock(lockId, int64(lockTimeout/time.Second), fn)
-			if !global.Continue(sleep) {
-				break
-			}
-		}
-	}
-}
-
-// 循环执行函数
-func (p *Startup) LoopWrapf(fn func(), sleep time.Duration) func() {
-	return func() {
-		for {
-			safex.Func(fn)
-			if !global.Continue(sleep) {
-				break
-			}
-		}
-	}
 }
 
 // 统一等待关闭
